7ed4b766c53275b0d76405af203f5bb0c4e39eb7
gabewebd <vlsqz.gabrielle@gmail.com>
2026-02-23 20:25:45 +0800

    update aria label read more

--- blogs.php
+++ blogs.php
@@ -656,7 +656,7 @@ include 'includes/db_connect.php';
                             </p>
                             <h3><?php echo $title; ?></h3>
                             <p><?php echo $excerpt; ?></p>
-                            <a href="/blog/<?php echo urlencode($slug); ?>" class="btn-read-more" aria-label="Read more about <?php echo $title; ?>">
+                            <a href="/blog/<?php echo urlencode($slug); ?>" class="btn-read-more" aria-label="Read the full article: <?php echo $title; ?>">
                                 Read More <i class="fas fa-arrow-right"></i>
                             </a>
                         </div>
