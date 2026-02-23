d505274cb229541cf51d34ae7aeba4689d6ac6ba
gabewebd <vlsqz.gabrielle@gmail.com>
2026-02-23 20:10:20 +0800

    add aria-label for blogs php

--- blogs.php
+++ blogs.php
@@ -656,7 +656,7 @@ include 'includes/db_connect.php';
                             </p>
                             <h3><?php echo $title; ?></h3>
                             <p><?php echo $excerpt; ?></p>
-                            <a href="/blog/<?php echo urlencode($slug); ?>" class="btn-read-more">
+                            <a href="/blog/<?php echo urlencode($slug); ?>" class="btn-read-more" aria-label="Read more about <?php echo $title; ?>">
                                 Read More <i class="fas fa-arrow-right"></i>
                             </a>
                         </div>
